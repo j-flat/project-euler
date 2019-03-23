d1d6b537b4475fb11a4047a7f7883836be6e8775
Joni Kämppä <joni.kamppa@sc5.io>
2019-03-23 14:22:39 +0100

    start working on type validator

--- javascript/src/helpers/helpers.js
+++ javascript/src/helpers/helpers.js
@@ -1,5 +1,10 @@
-const isRequired = (paam) => { 
-  throw new Error("param is required!")
+const isRequired = (param = undefined) => { 
+  throw new Error(`param ${(param ? ": " + param : "")} is required!`)
 }
 
-module.exports = isRequired;
+const validate = (type = isRequired(), isTypeOf = isRequired()) => {
+  return 0;
+}
+
+module.exports.isRequired = isRequired;
+module.exports.validate = validate;
